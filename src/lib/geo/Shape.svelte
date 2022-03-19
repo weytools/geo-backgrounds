1d35b6769223ca0140451781245f4ce182e892f8
Isaac <weytools@gmail.com>
2022-03-19 12:06:10 -0400

    bg work

---
+++ src/lib/geo/Shape.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Square from './Square.svelte';
+
+    export let geo;
+
+
+    // Event Handlers
+    let hovering;
+
+    function enter() {
+        hovering = true;
+    }
+
+    function leave() {
+        hovering = false;
+    }
+</script>
+
+<svelte:component this={selected.component} on:mouseenter={enter} on:mouseleave={leave} /> 
+
+<style>
+</style>
